d62b938d16b909d910d995448ca2efb8373bab04
Adryan Almeida <adryan1990@gmail.com>
2025-03-24 11:51:55 -0300

    fix: msg overrides

--- src/main.tsx
+++ src/main.tsx
@@ -4,34 +4,34 @@ import { KcPage } from "./kc.gen";
 
 // The following block can be uncommented to test a specific page with `yarn dev`
 // Don't forget to comment back or your bundle size will increase
-// import { getKcContextMock } from "./login/KcPageStory";
-//
-// if (import.meta.env.DEV) {
-//   window.kcContext = getKcContextMock({
-//     pageId: "login.ftl",
-//     overrides: {
-//       login: {
-//         username: "demo@automining.com.br",
-//       },
-//       locale: {
-//         currentLanguageTag: "pt-BR",
-//       },
-//       social: {
-//         providers: [
-//           {
-//             alias: "microsoft",
-//             displayName: "Microsoft"
-//           }
-//         ]
-//       },
-//       registrationDisabled: true,
-//       message: {
-//         type: "error",
-//         summary: "Senha inválida",
-//       }
-//     },
-//   });
-// }
+import { getKcContextMock } from "./login/KcPageStory";
+
+if (import.meta.env.DEV) {
+  window.kcContext = getKcContextMock({
+    pageId: "login.ftl",
+    overrides: {
+      login: {
+        username: "demo@automining.com.br",
+      },
+      locale: {
+        currentLanguageTag: "pt-BR",
+      },
+      social: {
+        providers: [
+          {
+            alias: "microsoft",
+            displayName: "Microsoft"
+          }
+        ]
+      },
+      registrationDisabled: true,
+      message: {
+        type: "error",
+        summary: "Senha inválida",
+      }
+    },
+  });
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,12 @@ export default defineConfig({
     keycloakify({
       accountThemeImplementation: "none",
       themeName: process.env.COMPANY ?? "automining",
+      kcContextExclusionsFtl: [
+        '<@addToXKeycloakifyMessagesIfMessageKey str="welcomeMessage" />',
+        '<@addToXKeycloakifyMessagesIfMessageKey str="appName" />',
+        '<@addToXKeycloakifyMessagesIfMessageKey str="companyMotto" />',
+        '<@addToXKeycloakifyMessagesIfMessageKey str="companyLogoName" />',
+      ].join("\n")
     }),
   ]
 });

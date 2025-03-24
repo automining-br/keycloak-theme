1774db4715fa7904c1cca479ae223478d8973b73
Adryan Almeida <adryan1990@gmail.com>
2025-03-24 10:34:41 -0300

    fix(ts): unused vars

--- src/login/pages/Login.tsx
+++ src/login/pages/Login.tsx
@@ -53,7 +53,7 @@ export default function Login(props: PageProps<Extract<KcContext, { pageId: "log
                 <div className={clsx("flex", "w-full", "border-t", "border-gray-200")} />
               </div>
               <ul>
-                {social.providers.map((...[p, , providers]) => (
+                {social.providers.map((...[p, , _providers]) => (
                   <li key={p.alias}>
                     <a
                       href={p.loginUrl}
@@ -223,28 +223,5 @@ export default function Login(props: PageProps<Extract<KcContext, { pageId: "log
         </div>
       </div>
     </Template >
-  );
-}
-
-function PasswordWrapper(props: { kcClsx: KcClsx; i18n: I18n; passwordInputId: string; children: JSX.Element }) {
-  const { kcClsx, i18n, passwordInputId, children } = props;
-
-  const { msgStr } = i18n;
-
-  const { isPasswordRevealed, toggleIsPasswordRevealed } = useIsPasswordRevealed({ passwordInputId });
-
-  return (
-    <div className={kcClsx("kcInputGroup")}>
-      {children}
-      <button
-        type="button"
-        className={kcClsx("kcFormPasswordVisibilityButtonClass")}
-        aria-label={msgStr(isPasswordRevealed ? "hidePassword" : "showPassword")}
-        aria-controls={passwordInputId}
-        onClick={toggleIsPasswordRevealed}
-      >
-        <i className={kcClsx(isPasswordRevealed ? "kcFormPasswordVisibilityIconHide" : "kcFormPasswordVisibilityIconShow")} aria-hidden />
-      </button>
-    </div>
   );
 }

19cb7f75d2b0efe4a35ec96b16d56d2082d89006
Adryan Almeida <adryan1990@gmail.com>
2025-03-24 11:29:15 -0300

    chore(ci): fix messages

--- src/login/i18n.ts
+++ src/login/i18n.ts
@@ -5,14 +5,6 @@ import type { ThemeName } from "../kc.gen";
 /** @see: https://docs.keycloakify.dev/features/i18n */
 const { useI18n, ofTypeI18n } = i18nBuilder
   .withThemeName<ThemeName>()
-  .withCustomTranslations({
-    "pt-BR": {
-      "companyLogoName": "automining",
-      "welcomeMessage": "Bem-vindo ao",
-      "appName": "Autominig Login",
-      "companyMotto": "Nossa missão é fazer uma diferença positiva, desenvolvendo recursos naturais para melhorar a vida das pessoas agora e para as gerações futuras.",
-    }
-  })
   .build();
 
 type I18n = typeof ofTypeI18n;
